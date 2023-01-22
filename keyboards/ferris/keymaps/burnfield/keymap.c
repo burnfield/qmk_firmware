9ec64ae87d93edfd167368e095e2acb274ae78db
Oskar <oskar.branfeldt@gmail.com>
2023-01-22 16:28:37 +0100

    working ferris, two sided layers

--- keyboards/ferris/keymaps/burnfield/keymap.c
+++ keyboards/ferris/keymaps/burnfield/keymap.c
@@ -1,139 +1,26 @@
 #include QMK_KEYBOARD_H
 #include "features/custom_shift_keys.h"
-#include "keymap_swedish.hh"
 enum layers {
   _QWE = 0,
   _NAV,
-  _SYM,
+  _FN,
+  _SYM0,
+  _SYM1,
   _OSL,
   _I3SW,
   _I3MV,
 };
 
 enum custom_keycodes {
-  MT_EXLM = SAFE_RANGE,
-  MT_BSLS,
-  MT_PIPE,
-  MT_QUOT,
-  MT_EQL,
-  MT_UNDS,
-  MT_MINS,
-  MT_RPRN,
-  MT_SCLN,
-  SE_GRV,
-  MT_CIRC,
-  MT_TILD,
-  MT_TMUX,
+  MT_TMUX = SAFE_RANGE,
 };
 
-#define HM_EXLM LGUI_T(MT_EXLM)
-#define HM_BSLS LALT_T(MT_BSLS)
-#define HM_PIPE LSFT_T(MT_PIPE)
-#define HM_QUOT LCTL_T(MT_QUOT)
-#define HM_EQL RCTL_T(MT_EQL)
-#define HM_UNDS RSFT_T(MT_UNDS)
-#define HM_MINS LALT_T(MT_MINS)
-#define HM_RPRN RGUI_T(MT_RPRN)
-#define HM_SCLN RGUI_T(MT_SCLN)
-
-const custom_shift_key_t custom_shift_keys[] = {
-    {RSFT_T(SE_COMM), SE_LABK}, // Shift , is !
-    {LALT_T(SE_DOT), SE_RABK},  // Shift . is ?
-    {HM_SCLN, SE_COLN},         // Shift : is ;
-    {SE_SLSH, SE_QUES},         // Shift : is ;
-};
-uint8_t NUM_CUSTOM_SHIFT_KEYS =
-    sizeof(custom_shift_keys) / sizeof(custom_shift_key_t);
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-  case HM_EQL:
-    if (record->tap.count > 0) {
-      if (record->event.pressed) {
-        tap_code16(SE_EQL);
-      }
-      return false;
-    }
-  case HM_UNDS:
-    if (record->tap.count > 0) {
-      if (record->event.pressed) {
-        tap_code16(SE_UNDS);
-      }
-      return false;
-    }
-  case HM_MINS:
-    if (record->tap.count > 0) {
-      if (record->event.pressed) {
-        tap_code16(SE_MINS);
-      }
-      return false;
-    }
-  case HM_RPRN:
-    if (record->tap.count > 0) {
-      if (record->event.pressed) {
-        tap_code16(SE_RPRN);
-      }
-      return false;
-    }
-  case HM_EXLM:
-    if (record->tap.count > 0) {
-      if (record->event.pressed) {
-        tap_code16(SE_EXLM);
-      }
-      return false;
-    }
-  case HM_BSLS:
-    if (record->tap.count > 0) {
-      if (record->event.pressed) {
-        tap_code16(SE_BSLS);
-      }
-      return false;
-    }
-  case HM_PIPE:
-    if (record->tap.count > 0) {
-      if (record->event.pressed) {
-        tap_code16(SE_PIPE);
-      }
-      return false;
-    }
-  case HM_QUOT:
-    if (record->tap.count > 0) {
-      if (record->event.pressed) {
-        tap_code16(SE_QUOT);
-      }
-      return false;
-    }
-  case HM_SCLN:
-    if (record->tap.count > 0) {
-      if (record->event.pressed) {
-        tap_code16(SE_SCLN);
-      }
-      return false;
-    }
-  case SE_GRV:
-    if (record->event.pressed) {
-      tap_code16(KC_PLUS);
-      tap_code16(KC_PLUS);
-      tap_code16(KC_BSPC);
-    }
-    return false;
-  case MT_TILD:
-    if (record->event.pressed) {
-      tap_code16(SE_TILD);
-      tap_code16(SE_TILD);
-      tap_code16(KC_BSPC);
-    }
-    return false;
-  case MT_CIRC:
-    if (record->event.pressed) {
-      tap_code16(SE_CIRC);
-      tap_code16(SE_CIRC);
-      tap_code16(KC_BSPC);
-    }
-    return false;
   case MT_TMUX:
     if (record->event.pressed) {
-      tap_code16(LCTL(SE_Q));
+      tap_code16(RCTL(KC_Q));
       set_oneshot_layer(_OSL, ONESHOT_START);
     }
     return false;
@@ -144,133 +31,126 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return false;
   }
-  if (!process_custom_shift_keys(keycode, record)) {
-    return false;
-  }
   return true;
 };
 
+// Left side
+#define KC_GUIA LGUI_T(KC_A)
+#define KC_ALTS LALT_T(KC_S)
+#define KC_SFTD LSFT_T(KC_D)
+#define KC_CTLF RCTL_T(KC_F)
+
+// Right side qwerty
+#define KC_ALTL LALT_T(KC_L)
+#define KC_SFTK RSFT_T(KC_K)
+#define KC_CTLJ RCTL_T(KC_J)
+#define KC_GUISCLN LGUI_T(KC_SCLN)
+
 enum combos {
-  WE_ESC,
-  SD_TAB,
-  XC_CLBR,
-  CV_LPRN,
-  KL_ENT,
-  IO_TMUX,
-  UI_I3SW,
-  YU_I3MV,
-  PERDAS_ODIA,
-  PERDAS_ADIA,
-  PERDAS_ARNG,
-  COMPER_CRBR,
-  MCOM_RBRC,
+  Q_W_ESC,
+  S_D_TAB,
+  X_C_LBRC,
+  C_V_LCBR,
+  K_L_ENT,
+  I_O_TMUX,
+  U_I_I3SW,
+  Y_U_I3MV,
+  COMM_DOT_RBRC,
+  M_COMM_RCBR,
 };
-const uint16_t PROGMEM we_combo[] = {SE_W, SE_E, COMBO_END};
-const uint16_t PROGMEM sd_combo[] = {SE_S, SE_D, COMBO_END};
 
-const uint16_t PROGMEM xc_combo[] = {LALT_T(SE_X), LSFT_T(SE_C), COMBO_END};
-const uint16_t PROGMEM cv_combo[] = {LSFT_T(SE_C), LCTL_T(SE_V), COMBO_END};
+const uint16_t PROGMEM w_e_combo[] = {KC_W, KC_E, COMBO_END};
+const uint16_t PROGMEM s_d_combo[] = {KC_ALTS, KC_SFTD, COMBO_END};
 
-const uint16_t PROGMEM io_combo[] = {SE_I, SE_O, COMBO_END};
-const uint16_t PROGMEM ui_combo[] = {SE_U, SE_I, COMBO_END};
-const uint16_t PROGMEM yu_combo[] = {SE_Y, SE_U, COMBO_END};
-const uint16_t PROGMEM kl_combo[] = {SE_K, SE_L, COMBO_END};
+const uint16_t PROGMEM x_c_combo[] = {KC_X, KC_C, COMBO_END};
+const uint16_t PROGMEM c_v_combo[] = {KC_C, KC_V, COMBO_END};
 
-const uint16_t PROGMEM perdas_combo[] = {SE_SLSH, LALT_T(SE_DOT), COMBO_END};
-const uint16_t PROGMEM perdas0_combo[] = {SE_L, HM_SCLN, COMBO_END};
-const uint16_t PROGMEM perdas1_combo[] = {SE_O, SE_P, COMBO_END};
+const uint16_t PROGMEM i_o_combo[] = {KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM u_i_combo[] = {KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM y_u_combo[] = {KC_Y, KC_U, COMBO_END};
+const uint16_t PROGMEM k_l_combo[] = {KC_SFTK, KC_ALTL, COMBO_END};
 
-const uint16_t PROGMEM comper_combo[] = {LALT_T(SE_DOT), RSFT_T(SE_COMM),
-                                         COMBO_END};
-const uint16_t PROGMEM mcom_combo[] = {RSFT_T(SE_COMM), RCTL_T(SE_M),
-                                       COMBO_END};
+const uint16_t PROGMEM comm_dot_combo[] = {KC_DOT, KC_COMM, COMBO_END};
+const uint16_t PROGMEM m_comm_combo[] = {KC_COMM, KC_M, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-    [WE_ESC] = COMBO(we_combo, KC_ESC),
-    [SD_TAB] = COMBO(sd_combo, KC_TAB),
+    [Q_W_ESC] = COMBO(w_e_combo, KC_ESC),
+    [S_D_TAB] = COMBO(s_d_combo, KC_TAB),
 
-    [XC_CLBR] = COMBO(xc_combo, SE_LBRC),
-    [CV_LPRN] = COMBO(cv_combo, SE_LCBR),
+    [X_C_LBRC] = COMBO(x_c_combo, KC_LBRC),
+    [C_V_LCBR] = COMBO(c_v_combo, KC_LCBR),
 
-    [KL_ENT] = COMBO(kl_combo, KC_ENT),
-    [IO_TMUX] = COMBO(io_combo, MT_TMUX),
-    [UI_I3SW] = COMBO(ui_combo, OSL(_I3SW)),
-    [YU_I3MV] = COMBO(yu_combo, OSL(_I3MV)),
+    [K_L_ENT] = COMBO(k_l_combo, KC_ENT),
+    [I_O_TMUX] = COMBO(i_o_combo, MT_TMUX),
+    [U_I_I3SW] = COMBO(u_i_combo, OSL(_I3SW)),
+    [Y_U_I3MV] = COMBO(y_u_combo, OSL(_I3MV)),
 
-    [PERDAS_ODIA] = COMBO(perdas_combo, SE_ODIA),
-    [PERDAS_ADIA] = COMBO(perdas0_combo, SE_ADIA),
-    [PERDAS_ARNG] = COMBO(perdas1_combo, SE_ARNG),
-
-    [COMPER_CRBR] = COMBO(comper_combo, SE_RBRC),
-    [MCOM_RBRC] = COMBO(mcom_combo, SE_RCBR),
+    [COMM_DOT_RBRC] = COMBO(comm_dot_combo, KC_RBRC),
+    [M_COMM_RCBR] = COMBO(m_comm_combo, KC_RCBR),
 };
 
 // Aliases for readability
 #define QWE DF(_QWE)
-#define SYM MO(_SYM)
-#define NAV MO(_NAV)
-
-// Left side
-#define SE_GUIA LGUI_T(SE_A)
-#define SE_ALTX LALT_T(SE_X)
-#define SE_SFTC LSFT_T(SE_C)
-#define SE_CTLV LCTL_T(SE_V)
-
-#define LGUI_F5 LGUI_T(KC_F5)
-#define LALT_F2 LALT_T(KC_F2)
-#define LSFT_F3 LSFT_T(KC_F3)
-#define LCTL_F4 LCTL_T(KC_F4)
-
-// Right side qwerty
-#define SE_ALTD LALT_T(SE_DOT)
-#define SE_SFTM RSFT_T(SE_COMM)
-#define SE_CTLM RCTL_T(SE_M)
-#define CTL_LFT RCTL(KC_LEFT)
-#define CTL_RGT RCTL(KC_RGHT)
+#define SYM0 MO(_SYM0)
+#define SYM1 MO(_SYM1)
+#define SPC_MV LT(_NAV, KC_SPC)
+#define BSPC_FN LT(_FN, KC_BSPC)
 
 // define i3 swap
-#define SW_1 LALT_T(KC_1)
-#define SW_2 LALT_T(KC_2)
-#define SW_3 LALT_T(KC_3)
-#define SW_4 LALT_T(KC_4)
-#define SW_5 LALT_T(KC_5)
-#define SW_6 LALT_T(KC_6)
-#define SW_7 LALT_T(KC_7)
-#define SW_8 LALT_T(KC_8)
-#define SW_9 LALT_T(KC_9)
-#define SW_0 LALT_T(KC_0)
+#define SW_1 LGUI(KC_1)
+#define SW_2 LGUI(KC_2)
+#define SW_3 LGUI(KC_3)
+#define SW_4 LGUI(KC_4)
+#define SW_5 LGUI(KC_5)
+#define SW_6 LGUI(KC_6)
+#define SW_7 LGUI(KC_7)
+#define SW_8 LGUI(KC_8)
+#define SW_9 LGUI(KC_9)
+#define SW_0 LGUI(KC_0)
 
 // define i3 swap
-#define MV_1 LSFT_T(LALT_T(KC_1))
-#define MV_2 LSFT_T(LALT_T(KC_2))
-#define MV_3 LSFT_T(LALT_T(KC_3))
-#define MV_4 LSFT_T(LALT_T(KC_4))
-#define MV_5 LSFT_T(LALT_T(KC_5))
-#define MV_6 LSFT_T(LALT_T(KC_6))
-#define MV_7 LSFT_T(LALT_T(KC_7))
-#define MV_8 LSFT_T(LALT_T(KC_8))
-#define MV_9 LSFT_T(LALT_T(KC_9))
-#define MV_0 LSFT_T(LALT_T(KC_0))
+#define MV_1 RSG(KC_1)
+#define MV_2 RSG(KC_2)
+#define MV_3 RSG(KC_3)
+#define MV_4 RSG(KC_4)
+#define MV_5 RSG(KC_5)
+#define MV_6 RSG(KC_6)
+#define MV_7 RSG(KC_7)
+#define MV_8 RSG(KC_8)
+#define MV_9 RSG(KC_9)
+#define MV_0 RSG(KC_0)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWE] = LAYOUT(
-    SE_Q,    SE_W,    SE_E,    SE_R,    SE_T,    /**/ SE_Y,    SE_U,    SE_I,    SE_O,    SE_P,
-    SE_GUIA, SE_S,    SE_D,    SE_F,    SE_G,    /**/ SE_H,    SE_J,    SE_K,    SE_L,    HM_SCLN,
-    SE_Z,    SE_ALTX, SE_SFTC, SE_CTLV, SE_B,    /**/ SE_N,    SE_CTLM, SE_SFTM, SE_ALTD, SE_SLSH,
-  						       KC_SPC,  NAV,     /**/ SYM,     KC_BSPC),
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, /**/ KC_Y, KC_U,    KC_I,    KC_O,    KC_P,
+    KC_GUIA, KC_ALTS, KC_SFTD, KC_CTLF, KC_G, /**/ KC_H, KC_CTLJ, KC_SFTK, KC_ALTL, KC_GUISCLN,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, /**/ KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+                               SPC_MV,  SYM1, /**/ SYM0, BSPC_FN),
 
-  [_SYM] = LAYOUT(
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    /**/ KC_6,    KC_7,    KC_8,    KC_9,     KC_0,
-    HM_EXLM, SE_AT,   SE_HASH, SE_DLR,  SE_PERC, /**/ MT_CIRC, SE_AMPR, SE_ASTR, SE_LPRN,  HM_RPRN,
-    SE_GRV,  HM_BSLS, HM_PIPE, HM_QUOT, S(SE_2), /**/ SE_PLUS, HM_EQL,  HM_UNDS, HM_MINS,  MT_TILD,
-                               _______, _______, /**/ _______, _______),
+  [_SYM0] = LAYOUT(
+    KC_1,    KC_2,    KC_3,    KC_4,       KC_5,    /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,     KC_PERC, /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+    KC_MINS, KC_UNDS, KC_QUOT, S(KC_QUOT), KC_BSLS, /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+                                  _______, KC_TRNS, /**/ _______, _______),
+
+  [_SYM1] = LAYOUT(
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, /**/ KC_6,    KC_7,    KC_8,    KC_9,     KC_0,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, /**/ KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, /**/ KC_PIPE, KC_PLUS,  KC_EQL, KC_GRV,  KC_TILD,
+                                _______, _______, /**/ KC_TRNS, KC_DEL),
 
   [_NAV] = LAYOUT(
-  	KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, /**/ KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
-  	LGUI_F5, KC_F6,   KC_F7,   KC_F8,   _______, /**/ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LGUI,
-  	KC_F1,   LALT_F2, LSFT_F3, LCTL_F4, _______, /**/ CTL_LFT, KC_RCTL, KC_RSFT, CTL_RGT, KC_LALT,
-  							   _______, _______, /**/ _______, KC_DEL),
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, /**/ KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, /**/ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                _______, _______, /**/ _______, _______),
+
+  [_FN] = LAYOUT(
+    KC_F1,  KC_F2, KC_F3,    KC_F4,   KC_F5,   /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+    KC_F6,  KC_F7, KC_F8,    KC_F9,   KC_F10,  /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+    KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+                             _______, _______, /**/ _______, _______),
 
   [_OSL] = LAYOUT(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    /**/ KC_6,    KC_7,    KC_8,    KC_9,     KC_0,
