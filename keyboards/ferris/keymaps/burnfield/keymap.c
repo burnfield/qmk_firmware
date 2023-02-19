10fdabf9cbcb75bdfa11148dab9532e65114ffab
Oskar <oskar.branfeldt@gmail.com>
2023-02-19 08:25:48 +0100

    sweep: lowered combo time

--- keyboards/ferris/keymaps/burnfield/keymap.c
+++ keyboards/ferris/keymaps/burnfield/keymap.c
@@ -4,8 +4,7 @@ enum layers {
   _QWE = 0,
   _NAV,
   _FN,
-  _SYM0,
-  _SYM1,
+  _SYM,
   _OSL,
   _I3SW,
   _I3MV,
@@ -90,11 +89,9 @@ combo_t key_combos[COMBO_COUNT] = {
 };
 
 // Aliases for readability
-#define QWE DF(_QWE)
-#define SYM0 MO(_SYM0)
-#define SYM1 MO(_SYM1)
-#define SPC_MV LT(_NAV, KC_SPC)
-#define BSPC_FN LT(_FN, KC_BSPC)
+#define SYM MO(_SYM)
+#define NAV MO(_NAV)
+#define FN MO(_FN)
 
 // define i3 swap
 #define SW_1 LGUI(KC_1)
@@ -126,25 +123,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, /**/ KC_Y, KC_U,    KC_I,    KC_O,    KC_P,
     KC_GUIA, KC_ALTS, KC_SFTD, KC_CTLF, KC_G, /**/ KC_H, KC_CTLJ, KC_SFTK, KC_ALTL, KC_GUISCLN,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, /**/ KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                               SPC_MV,  SYM1, /**/ SYM0, BSPC_FN),
+                               KC_SPC,  NAV,  /**/ SYM,  KC_BSPC),
 
-  [_SYM0] = LAYOUT(
-    KC_1,    KC_2,    KC_3,    KC_4,       KC_5,    /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,     KC_PERC, /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-    KC_MINS, KC_UNDS, KC_QUOT, S(KC_QUOT), KC_BSLS, /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-                                  _______, KC_TRNS, /**/ _______, _______),
-
-  [_SYM1] = LAYOUT(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, /**/ KC_6,    KC_7,    KC_8,    KC_9,     KC_0,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, /**/ KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, /**/ KC_PIPE, KC_PLUS,  KC_EQL, KC_GRV,  KC_TILD,
-                                _______, _______, /**/ KC_TRNS, KC_DEL),
+  [_SYM] = LAYOUT(
+    KC_1,    KC_2,    KC_3,    KC_4,       KC_5,    /**/ KC_6,    KC_7,    KC_8,    KC_9,     KC_0,
+    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,     KC_PERC, /**/ KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN,
+    KC_MINS, KC_UNDS, KC_QUOT, S(KC_QUOT), KC_BSLS, /**/ KC_PIPE, KC_PLUS,  KC_EQL, KC_GRV,  KC_TILD,
+                               KC_TRNS,    _______, /**/ KC_TRNS, KC_TRNS),
 
   [_NAV] = LAYOUT(
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, /**/ KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, /**/ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                _______, _______, /**/ _______, _______),
+                               FN,       _______, /**/ _______, KC_DEL),
 
   [_FN] = LAYOUT(
     KC_F1,  KC_F2, KC_F3,    KC_F4,   KC_F5,   /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
