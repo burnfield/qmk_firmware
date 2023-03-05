e36eaef87bbf712583f88481982a79b358360d46
Oskar <oskar.branfeldt@gmail.com>
2023-03-05 10:40:20 +0100

    kyria callum osm

--- keyboards/splitkb/kyria/keymaps/burnfield/config.h
+++ keyboards/splitkb/kyria/keymaps/burnfield/config.h
@@ -16,13 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-// Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 200
-#define PERMISSIVE_HOLD
-#define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_FORCE_HOLD
 
-#define COMBO_COUNT 14
 // Underglow configuration
 #ifdef RGBLIGHT_ENABLE
   #define RGBLIGHT_ANIMATIONS
